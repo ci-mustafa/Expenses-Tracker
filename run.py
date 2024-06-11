720af0464d28d5e5e12868fb042c9484eef620a2
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-06-11 14:34:14 +0000

    feat: Add select_sheet function to give the user ability to select a sheet from spreadsheet

--- run.py
+++ run.py
@@ -14,16 +14,50 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Expenses tracker')
 
-# incomes = SHEET.worksheet('Incomes')
 
-# data = incomes.get_all_values()
+def select_sheet():
+    """
+    This function helps you choose which sheet to access from your Google Spreadsheet.
 
-# pprint(data)
+    It will keep asking you to pick an option until you give a valid choice. Here are the options:
+    - Press "1" for the "Incomes" sheet
+    - Press "2" for the "Expenses" sheet
+    - Press "3" for the "Summary" sheet
+
+    Once you make a choice, the function will select the corresponding sheet, let you know it's selected,
+    and then return that sheet so you can work with it.
+
+    Returns:
+        The worksheet object you selected from the Google Spreadsheet.
+    """
+    while True:
+        choice = input("Please select the sheet you want to access by entering the corresponding number: 1 for Incomes, 2 for Expenses, or 3 for Summary: \n")
+        if choice in ["1", "2", "3"]:
+            if choice == "1":
+                print("Selecting Incomes sheet...")
+                incomes = SHEET.worksheet("Incomes")
+                print("Incomes sheet selected.")
+                return incomes
+            elif choice == "2":
+                print("Selecting Expenses sheet...")
+                expenses = SHEET.worksheet("Expenses")
+                print("Expenses sheet selected.")
+                return expenses
+            else:
+                print("Selecting Summary sheet...")
+                summaries = SHEET.worksheet("Summary")
+                print("Summary sheet selected.")
+                return summaries
+            break
+        else:
+            print("Wrong Entry!!!")
 
 def main():
     """
     Run all program functions
     """
     print("****Welcome to Expenses Tracker! We're here to help you track your expenses and incomes.****\n****Let's begin your financial journey.****")
+
+    select_sheet()
 
 main()
