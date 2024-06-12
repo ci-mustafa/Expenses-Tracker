0cd2b5bcf158faf86f8f47e9cdfab874778a0a39
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-06-12 10:42:45 +0000

    feat: add modify_or_display_sheet function to show the sheet content and add data to the sheet

--- run.py
+++ run.py
@@ -37,20 +37,66 @@ def select_sheet():
                 print("Selecting Incomes sheet...")
                 incomes = SHEET.worksheet("Incomes")
                 print("Incomes sheet selected.")
+                print("")
                 return incomes
             elif choice == "2":
                 print("Selecting Expenses sheet...")
                 expenses = SHEET.worksheet("Expenses")
                 print("Expenses sheet selected.")
+                print("")
                 return expenses
             else:
                 print("Selecting Summary sheet...")
                 summaries = SHEET.worksheet("Summary")
                 print("Summary sheet selected.")
+                print("")
                 return summaries
             break
         else:
             print("Wrong Entry!!!")
+
+def modify_or_display_sheet(sheet):
+    """
+    This function allows you to either show the sheet data or add new data to the sheet.
+
+    Parameters:
+        sheet: The worksheet object you want to manage.
+
+    The function will prompt you to choose between viewing the current data or adding new data.
+    """
+    print("Select an action: view the sheet's data or add new data?\n"
+          "Type 'info' to see the data or 'add' to enter new data.\n"
+          "If you want to exit the program type 'exit'.\n"
+          )
+    
+    while True:
+        user_choice = input(">>  ").lower()
+        if user_choice in ["info", "add", "exit"]:
+            if user_choice == "info":
+                data = sheet.get_all_values()
+                print("Presenting the data...")
+                print("")
+                pprint(data)
+                print("")
+                if sheet.title != "Summary":
+                    print("Current data shown. Type 'add' to add more data or 'exit' to leave the program.")
+                print("Current data shown. Type 'exit' to leave the program.")
+            elif user_choice == "add":
+                # if sheet.title != "Summary":
+                #     print("Adding data...")
+                #     print("")
+                if sheet.title == "Summary":
+                    print("Heads up: This sheet does not support manual data addition.\n"
+                          "This sheet is read-only. Type 'info' to see its contents."
+                          )
+                elif sheet.title == "Incomes":
+                    pass             
+            else:
+                print("Exiting the program...\nProgram cloesed.")
+                break
+        else:
+            print("Wrong Entry!!!\nPlease type 'info', 'add' or 'exit'.")
+        
 
 def main():
     """
@@ -58,6 +104,6 @@ def main():
     """
     print("****Welcome to Expenses Tracker! We're here to help you track your expenses and incomes.****\n****Let's begin your financial journey.****")
 
-    select_sheet()
+    modify_or_display_sheet(select_sheet())
 
 main()
